018b929867af3ae02b8e2bf174c3a0a1e8d12656
jmontoya05 <santiagomontoya997@gmail.com>
2024-03-13 14:37:03 -0500

    Connection to telegarm and first message

--- src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
+++ src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -7,8 +7,8 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
-    public static final String NAME = "agregue el nombre del bot entre comillas a continuación";
-    public static final String TOKEN = "agregue el token del bot entre comillas a continuación";
+    public static final String NAME = "jmontoyaBot";
+    public static final String TOKEN = "6351553874:AAHNQG4Vlh-aSoF2RlwzRD-b5ELVxdL9u_c";
 
     public MyFirstTelegramBot() {
         super(NAME, TOKEN);
@@ -16,9 +16,17 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
     @Override
     public void onUpdateEventReceived(Update update) {
-        System.out.println("Ok");
+        //TODO: Main operation here
+        if (getMessageText().equals("/start")){
+            sendTextMessageAsync("Hi, future programmer");
+        }
+        if (getMessageText().toLowerCase().contains("hi")) {
+            sendTextMessageAsync("Hi, what's your name?");
+        }
+        if (getMessageText().toLowerCase().contains("my name is")){
+            sendTextMessageAsync("Nice to meet you, I am *Cat*");
+        }
     }
-
     public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         telegramBotsApi.registerBot(new MyFirstTelegramBot());
